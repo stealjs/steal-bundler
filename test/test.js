b49db4745bcd3a10d7151e795a1719fc84fb871c
Julian <julian@MacBook-Pro.local>
2017-09-22 13:54:44 +0200

    remove race conditions

--- test/test.js
+++ test/test.js
@@ -11,6 +11,7 @@ var sinon = require('sinon');
 var copySpy;
 
 describe("inferred from source content", function(){
+	this.timeout(5000);
 
 	before(function(done){
 		copySpy = sinon.spy(fs, "copy");
@@ -38,7 +39,7 @@ describe("inferred from source content", function(){
 
 	it("moves assets it finds in css", function(){
 		assert(
-			exists(__dirname + "/basics/dist/images/logo.png"),
+			exists.file(__dirname + "/basics/dist/images/logo.png"),
 			"logo moved to the destination folder"
 		);
 	});
@@ -61,15 +62,15 @@ describe("inferred from source content", function(){
 
 	it("works with font-face", function(){
 		assert(
-			exists(__dirname + "/basics/dist/fonts/foo.eot"),
+			exists.file(__dirname + "/basics/dist/fonts/foo.eot"),
 			"eot font moved"
 		);
 		assert(
-			exists(__dirname + "/basics/dist/fonts/foo.woff"),
+			exists.file(__dirname + "/basics/dist/fonts/foo.woff"),
 			"woff font moved"
 		);
 		assert(
-			exists(__dirname + "/basics/dist/fonts/foo.svg"),
+			exists.file(__dirname + "/basics/dist/fonts/foo.svg"),
 			"svg font moved"
 		);
 	});
@@ -78,23 +79,18 @@ describe("inferred from source content", function(){
 
 describe("provided as a glob", function(){
 	before(function(done){
-		rimraf(__dirname + "/basics/dist").then(function(){
-			this.buildPromise = stealTools.build({
-				config: __dirname + "/basics/package.json!npm"
-			}, {
-				quiet: true
+		fs.removeSync(__dirname + "/basics/dist");
+
+		const buildPromise = stealTools.build({
+			config: __dirname + "/basics/package.json!npm"
+		}, {
+			quiet: true
+		}).then(function (buildResult) {
+			return bundleAssets(buildResult, {
+				infer: false,
+				glob: ["test/basics/docs/**/*", "test/basics/fonts/*.woff"]
 			});
-
-			this.bundlePromise = this.buildPromise.then(function(buildResult){
-				return bundleAssets(buildResult, {
-					infer: false,
-					glob: ["test/basics/docs/**/*", "test/basics/fonts/*.woff"]
-				});
-			});
-
-			return this.bundlePromise;
-
-		}.bind(this)).then(function(){
+		}).then(function (builtResult) {
 			done();
 		});
 	});
@@ -110,37 +106,30 @@ describe("provided as a glob", function(){
 	});
 
 	it("infer: false prevents copying of css images", function(){
-		assert(!exists(__dirname + "/basics/dist/images"), "images not copied");
+		assert(!exists.directory(__dirname + "/basics/dist/images"), "images not copied");
 	});
 });
 
 describe("when globbing a directory that contains files", function(){
 	before(function(done){
-		rimraf(__dirname + "/basics/dist").then(function(){
-			this.buildPromise = stealTools.build({
-				config: __dirname + "/basics/package.json!npm"
-			}, {
-				quiet: true
-			});
+		fs.removeSync(__dirname + "/basics/dist");
 
-			this.bundlePromise = this.buildPromise.then(function(buildResult){
-				return bundleAssets(buildResult, {
-					infer: false,
-					glob: "test/basics/docs/**"
-				});
+		const buildPromise = stealTools.build({
+			config: __dirname + "/basics/package.json!npm"
+		}, {
+			quiet: true
+		}).then(function (buildResult) {
+			return bundleAssets(buildResult, {
+				infer: false,
+				glob: "test/basics/docs/**"
 			});
-
-			return this.bundlePromise;
-		}.bind(this)).then(function(){
-			done();
-		}, function(err){
-			assert(!err, err.message);
+		}).then(function (builtResult) {
 			done();
 		});
 	});
 
 	it("JSON file was copied over", function(){
-		assert(exists(__dirname + "/basics/dist/docs/hello.json"), "doc copied");
+		assert(exists.file(__dirname + "/basics/dist/docs/hello.json"), "doc copied");
 	});
 });
 
